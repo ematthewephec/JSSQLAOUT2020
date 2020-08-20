6c30597115b30bcaf1a4e38e628560166a651594
ematthewephec <64141240+ematthewephec@users.noreply.github.com>
2020-08-21 00:39:26 +0200

    Add files via upload

--- frontend/js/aout.js
+++ frontend/js/aout.js
@@ -21,6 +21,11 @@ function goToManagment() {
     getElemId('managment').hidden = false;
     getElemId('addDevice').hidden = true;
     getElemId('deleteDevice').hidden = true;
+    getElemId('users').hidden = true;
+    getElemId('devices').hidden = true;
+    getElemId('divAffichageUser').hidden = true;
+    getElemId('usersDiv').hidden = true;
+    getElemId('types').hidden = true;
 
     return false
 }
@@ -44,4 +49,63 @@ function goToDeleteDevice() {
 function askConfirmation() {
     getElemId('confirmDelete').hidden = false;
 
+}
+
+function goToUsers() {
+    getElemId('accueil').hidden = true;
+    getElemId('connexion').hidden = true;
+    getElemId('managment').hidden = true;
+    getElemId('addDevice').hidden = true;
+    getElemId('deleteDevice').hidden = true;
+    getElemId('users').hidden = false;
+    getElemId('devices').hidden = true;
+    getElemId('usersDiv').hidden = false;
+
+}
+
+function goToDevices() {
+    getElemId('accueil').hidden = true;
+    getElemId('connexion').hidden = true;
+    getElemId('managment').hidden = true;
+    getElemId('addDevice').hidden = true;
+    getElemId('deleteDevice').hidden = true;
+    getElemId('users').hidden = true;
+    getElemId('devices').hidden = false;
+    getElemId('resultatRecherche').hidden = true;
+
+}
+
+function afficherResultat() {
+    getElemId('resultatRecherche').hidden = false;
+
+    return false;
+}
+
+function afficherDevicesUser() {
+    getElemId('divAffichageUser').hidden = false;
+    getElemId('usersDiv').hidden = true;
+}
+
+function goToTypes() {
+    getElemId('accueil').hidden = true;
+    getElemId('connexion').hidden = true;
+    getElemId('managment').hidden = true;
+    getElemId('addDevice').hidden = true;
+    getElemId('deleteDevice').hidden = true;
+    getElemId('users').hidden = true;
+    getElemId('devices').hidden = true;
+    getElemId('resultatRecherche').hidden = true;
+    getElemId('types').hidden = false;
+}
+
+function afficherTablets() {
+    getElemId("titreTableTypes").innerText = "Tablets";
+}
+
+function afficherComputers() {
+    getElemId("titreTableTypes").innerText = "Computers";
+}
+
+function afficherPhones() {
+    getElemId("titreTableTypes").innerText = "Phones";
 }
